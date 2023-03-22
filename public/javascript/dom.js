b1f10f14992f6e98848fb329d0d10f8d243b5ea8
momenmarwan <momenalgaga82@gmail.com>
2023-03-22 14:36:30 +0200

    make a card for a result

---
+++ public/javascript/dom.js
@@ -0,0 +1,12 @@
+
+
+// const createHtmlElement = (element , className , id) => {
+//     const ele = document.createElement(element)
+//     if(className){
+//         ele.className = className ;
+//     }
+//     if(id){
+//         ele.id = id
+//     }
+//     return ele
+// }
