c3fcefe266b1ead5706599ad03a088ca276b1e49
momenmarwan <momenalgaga82@gmail.com>
2023-03-22 09:30:51 +0200

    send filtered data to the browser

---
+++ src/router.js
@@ -0,0 +1,42 @@
+const { getDataFromFile , filterData} = require("./handler");
+const path = require("path");
+const url = require("url") ;
+
+const handler = (req, res) => {
+    const { pathname, query } = url.parse(req.url, true);
+
+    if (req.url == "/") {
+        const filePath = path.join(__dirname, "..", "public", "index.html");
+        getDataFromFile(filePath, (data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    });
+}
+    else if(req.url == "/public/css/style.css"){
+        const filePath = path.join(__dirname , ".." ,"public" , "css" , "style.css")
+        getDataFromFile(filePath , (data) => {
+            res.writeHead(200);
+            res.end(data);
+        })
+    }
+    else if(req.url == "/public/javascript/script.js"){
+        const filePath = path.join(__dirname , ".." ,"public" , "javascript" , "script.js")
+        getDataFromFile(filePath , (data) => {
+            res.writeHead(200);
+            res.end(data);
+        })
+    
+    }else if(req.url === `/autocomplete/data?key1=${query.key1}`) {
+        const filePath = path.join(__dirname , ".." , "src" , "data.json")
+        getDataFromFile(filePath , (data) => {
+            res.writeHead(200);
+            res.end(JSON.stringify(filterData(data , query.key1)))
+        })
+    }
+    else{
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("page not found");
+}
+};
+
+module.exports = handler;
